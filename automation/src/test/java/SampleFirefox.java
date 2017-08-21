78b01fa88d1133bb8466b02112a3a81ee659fee0
ravichitturi2675 <chitturiravi@hotmail.com>
2017-08-21 18:28:07 +0530

    Browser changed

--- automation/src/test/java/SampleFirefox.java
+++ automation/src/test/java/SampleFirefox.java
@@ -2,6 +2,8 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -19,8 +21,14 @@ public class SampleFirefox {
         //System.setProperty("phantomjs.binary.path","D:\\Works\\lib\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe");
       // driver = new PhantomJSDriver();
 
-        System.setProperty("webdriver.chrome.driver","D:\\Works\\lib\\chromedriver.exe");
-        driver = new ChromeDriver();
+       // System.setProperty("webdriver.chrome.driver","D:\\Works\\lib\\chromedriver.exe");
+
+       // DesiredCapabilities cap=new DesiredCapabilities();
+       // cap.setCapability("firefox_binary", "C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+        WebDriver driver = new FirefoxDriver();
+
+
+       // driver = new FirefoxDriver();
         driver.get("http://www.google.com");
 
         System.out.println("Page Title: "+ driver.getTitle());
